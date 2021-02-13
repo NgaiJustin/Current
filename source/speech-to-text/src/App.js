3730506fc0e86da33a8997b32ecb477ddb51c2e0
Justin Ngai <jn537@cornell.edu>
2021-02-13 15:22:21 -0500

    Implement Speech to Text functionality

---
+++ source/speech-to-text/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Speech from "./components/Speech";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Speech />
+            </header>
+        </div>
+    );
+}
+
+export default App;
