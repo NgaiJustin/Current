a767be694b1e435f8f6b43f3ed57f548f6e136be
luisCermeno <luis.cermeno.f@gmail.com>
2021-02-12 22:58:21 -0800

    Added createroom and roomview components

--- currentProject/src/BaseRouter.js
+++ currentProject/src/BaseRouter.js
@@ -6,6 +6,8 @@ import UploadVideo from "./containers/UploadVideoView";
 import VideoDetail from "./containers/VideoDetailView";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
+import CreateRoom from "./containers/CreateRoomView";
+import Room from "./containers/RoomView";
 
 const BaseRouter = () => (
     <div>
@@ -14,6 +16,8 @@ const BaseRouter = () => (
         <Route exact path="/login/" component={Login} />{" "}
         <Route exact path="/signup/" component={Signup} />{" "}
         <Route exact path="/upload/" component={UploadVideo} />{" "}
+        <Route exact path="/createroom/"  component={CreateRoom} />
+        <Route exact path="/room/:roomID" component={Room} />
     </div>
 );
 
