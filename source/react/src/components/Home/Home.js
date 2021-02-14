2e0f6bacc3926177e250a9db525b4c184111929c
Justin Ngai <jn537@cornell.edu>
2021-02-14 13:50:46 -0500

    Update Home.js

--- source/react/src/components/Home/Home.js
+++ source/react/src/components/Home/Home.js
@@ -1,34 +1,38 @@
 import React from "react";
 import { connect } from "react-redux";
 
-
 class Home extends React.Component {
     render() {
-        if (this.props.username != null)
-        {
+        if (this.props.username != null) {
             return (
                 <div>
                     <h1> Welcome, {this.props.username}. </h1>
                 </div>
             );
-        }
-        else {
+        } else {
             return (
                 <div>
-                    <h1> What is Current? </h1>
-                    <div>need to write a description here!!t</div>
+                    <h3 style={{ fontWeight: "bold" }}> What is Current 🌊?</h3>
+                    <div>
+                        Search and discover interesting conversations with real
+                        time analytics
+                    </div>
+                    <br></br>
+                    <br></br>
+
+                    <div>
+                        <h3 style={{ fontWeight: "bold" }}>Learn more here:</h3>
+                        <p>https://github.com/NgaiJustin/Current</p>
+                    </div>
                 </div>
             );
-
         }
-
     }
 }
 
-
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
     return {
-        username: state.username
+        username: state.username,
     };
 };
 
