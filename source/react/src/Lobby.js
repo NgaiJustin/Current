19d431c9660c285603a6d16516e353e0696b7c05
luisCermeno <luis.cermeno.f@gmail.com>
2021-02-14 01:59:29 -0800

    videohat now take authenticated user as input

--- source/react/src/Lobby.js
+++ source/react/src/Lobby.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 const Lobby = ({
-    username,
-    handleUsernameChange,
     roomName,
     handleRoomNameChange,
     handleSubmit,
@@ -11,18 +9,6 @@ const Lobby = ({
     return (
         <form onSubmit={handleSubmit}>
             <h2>Enter a room</h2>
-            <div>
-                <label htmlFor="name">Name:</label>
-                <input
-                    type="text"
-                    id="field"
-                    value={username}
-                    onChange={handleUsernameChange}
-                    readOnly={connecting}
-                    required
-                />
-            </div>
-
             <div>
                 <label htmlFor="room">Room name:</label>
                 <input
